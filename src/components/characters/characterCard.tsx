be1488c2146aa4c7ad8023ba0248a4e4f6414194
Victor Liew <victorliew0711@gmail.com>
2022-05-15 21:45:27 +0100

    adjusted characterCard

--- src/components/characters/characterCard.tsx
+++ src/components/characters/characterCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Link from '@mui/material/Link';
+// import { Link } from 'react-router-dom';
 import { styled } from '@mui/material';
 import { Grid } from '@mui/material';
 import { Box } from '@mui/system';
@@ -24,8 +25,6 @@ export const CharacterCard: React.FC<CharacterProps> = ({characters}: CharacterP
   
   return (
     <Box sx={{ flexGrow: 1, p: 4, top: 6}}>
-      <Link to={`/characters/${1}`}>
-        {/* cant find link to id? but can hardcode a character (i.e. 1 instead of id) */}
         <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
           {characters.map(item => {
             return (
@@ -37,7 +36,6 @@ export const CharacterCard: React.FC<CharacterProps> = ({characters}: CharacterP
             )
           })}
         </Grid>
-      </Link>
     </Box>
   )
 }
@@ -46,11 +44,13 @@ export const CharacterCard: React.FC<CharacterProps> = ({characters}: CharacterP
 const CharacterProfile: React.FC<CharacterCardVals> = ({id, image, name, status}: CharacterCardVals) => {
 
   return (
-    <Grid xs={4} sm={4} md={3} sx={{ display: 'flex', justifyContent: 'center', padding: 2 }}>
-      <Item>
-        <p>{name}</p>
-        <img width={180} height={180} src={image} alt={name}/>
-      </Item>
+    <Grid xs={4} sm={4} md={3} sx={{ display: 'flex', justifyContent: 'center', padding: 2 }} >
+      <Link href={`/characters/${id}`}>
+        <Item>
+          <p>{name}</p>
+          <img width={180} height={180} src={image} alt={name}/>
+        </Item>
+      </Link>
     </Grid>
   )
 }
